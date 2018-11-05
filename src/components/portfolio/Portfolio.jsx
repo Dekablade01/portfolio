c794da93758a81c57c79b67cd09e84310ce8d58d
Issarapong Poesua <dekablade01@gmail.com>
2018-11-06 01:18:14 +0700

    import json file

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,24 +1,10 @@
 import React from "react"
 import Hero from "./Hero/Hero"
+import Info from "../../info.json"
 
 class Portfolio extends React.Component {
-
-    state = { content: undefined }
-
-    componentDidMount() {
-        let url = "https://gist.githubusercontent.com/Dekablade01/299c7ae999df7588dbc7626656ba261c/raw/18a07099c4194e522e7596205cf07939f3ade413/cv.json"
-        fetch(url)
-            .then(response => response.json())
-            .then(data => { this.setState({ content: data }) })
-    }
-
     render() {
-
-        if (this.state.content === undefined) {
-            return <div />
-        }
-
-        return <Hero props={this.state.content.profile} />
+        return <Hero props={Info.profile} />
     }
 }
 export default Portfolio;
